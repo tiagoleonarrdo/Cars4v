6d37cbadc83674a9fe6103d2b8755309095c6db0
Tiago Leonardo <tiagoleonardo1408@gmail.com>
2024-11-14 17:45:29 -0300

    Configuração do modelo Car no Admin e criação do master template

--- aplicativo/admin.py
+++ aplicativo/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from aplicativo.models import Car
 
+class CarFilter(admin.ModelAdmin):
+    list_display = ("id", "model", "brand", "year")
+    list_display_links = ("id", "model", "brand", "year")
+    list_filter = ("model", "year")
+    search_fields = ("model", "brand")
+    
 # Register your models here.
-admin.site.register(Car)
+admin.site.register(Car, CarFilter)
