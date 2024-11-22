b498688e6e433ef18431e99ac6fc240923be3d3e
Tiago Leonardo <tiagoleonardo1408@gmail.com>
2024-11-22 16:04:26 -0300

    Atualizações 22/11

--- aplicativo/admin.py
+++ aplicativo/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from aplicativo.models import Car
 
 class CarFilter(admin.ModelAdmin):
-    list_display = ("id", "model", "brand", "year")
+    list_display = ("id", "user","model", "brand", "year")
     list_display_links = ("id", "model", "brand", "year")
     list_filter = ("model", "year")
     search_fields = ("model", "brand")
